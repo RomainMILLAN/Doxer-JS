a6a78549c9fb4ecb42a3f42287d89aa47bf1a8c4
Romain Millan <romainmillan34560@gmail.com>
2024-01-30 09:07:43 +0100

    🎨 | Explode and clean code

--- src/manager/ticketManager.ts
+++ src/manager/ticketManager.ts
@@ -5,6 +5,8 @@ import {
   Colors,
   EmbedBuilder,
   Guild,
+  Message,
+  TextChannel,
   User,
 } from "discord.js";
 import {
@@ -35,70 +37,96 @@ export async function createTicket(
   try {
     const ticketChannel = await createTicketChannelText(user, guild);
 
-    const closeTicket = new ButtonBuilder()
-      .setCustomId("ticketDelete")
-      .setLabel("❌ Suppression du ticket")
-      .setStyle(ButtonStyle.Danger);
-
-    const row: any = new ActionRowBuilder().addComponents(closeTicket);
-
-    const newTicketMessage = await ticketChannel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle(`🏷️ Ticket de ${user.displayName}`)
-          .setThumbnail(user.displayAvatarURL())
-          .setColor(Colors.LightGrey)
-          .setDescription(
-            `Bonjour, ${user.toString()} !\nBienvenue sur votre ticket, veuillez décrire votre problème et nous vous répondrons dès que possible.\nMerci de votre patience.\n\n> Le staff.`
-          )
-          .addFields({
-            name: "🕜 Date de création",
-            value: `Le ${getCurrentFormattedDateString()} à ${getCurrentFormattedTimeString()}`,
-            inline: true,
-          })
-          .addFields({
-            name: "➖ Prioritée du ticket",
-            value: `**${ticketPriority.value}**`,
-            inline: true,
-          })
-          .setTimestamp(),
-      ],
-      components: [row],
-    });
-
-    sentry(
-      user.client,
-      "Ticket",
-      whiteCheckMark + " Demande de la priorité pour la création d'un ticket",
+    const firstMessageTicket = await sendFirstMessageOfTicketChannel(
+      ticketChannel,
       user,
-      `/ticket priority:${priority}`
+      ticketPriority
     );
 
-    try {
-      const buttonAction = await newTicketMessage.awaitMessageComponent({
-        time: 3_600_000,
-      });
-
-      if (buttonAction.customId === "ticketDelete") {
-        sendDebug(`Suppression du ticket de ${user.tag}.`);
-
-        sentry(
-          buttonAction.user.client,
-          "Ticket",
-          `${whiteCheckMark} Suppression d'un ticket (${ticketChannel.name})`,
-          buttonAction.user
-        );
-
-        deleteTicketChannelText(ticketChannel, user);
-
-        return;
-      }
-    } catch (e) {
-      sendError(`Une erreur est survenur sur les actions des tickets (${e}).`);
-    }
+    actionTicketButton(firstMessageTicket, user, ticketChannel);
   } catch (error) {
     sendError(
       `Une erreur est survenue lors de la création du ticket (${error}).`
     );
+  }
+}
+
+async function sendFirstMessageOfTicketChannel(
+  ticketChannel: TextChannel,
+  user: User,
+  priority: { key: string; value: string }
+): Promise<Message<boolean>> {
+  const newTicketMessage = await ticketChannel.send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle(`🏷️ Ticket de ${user.displayName}`)
+        .setThumbnail(user.displayAvatarURL())
+        .setColor(Colors.LightGrey)
+        .setDescription(
+          `Bonjour, ${user.toString()} !\nBienvenue sur votre ticket, veuillez décrire votre problème et nous vous répondrons dès que possible.\nMerci de votre patience.\n\n> Le staff.`
+        )
+        .addFields({
+          name: "🕜 Date de création",
+          value: `Le ${getCurrentFormattedDateString()} à ${getCurrentFormattedTimeString()}`,
+          inline: true,
+        })
+        .addFields({
+          name: "➖ Prioritée du ticket",
+          value: `**${priority.value}**`,
+          inline: true,
+        })
+        .setTimestamp(),
+    ],
+    components: [createTicketButton()],
+  });
+
+  sentry(
+    user.client,
+    "Ticket",
+    whiteCheckMark + " Demande de la priorité pour la création d'un ticket",
+    user,
+    `/ticket priority:${priority.key}`
+  );
+
+  return newTicketMessage;
+}
+
+function createTicketButton() {
+  const closeTicket = new ButtonBuilder()
+    .setCustomId("ticketDelete")
+    .setLabel("❌ Suppression du ticket")
+    .setStyle(ButtonStyle.Danger);
+
+  const row: any = new ActionRowBuilder().addComponents(closeTicket);
+
+  return row;
+}
+
+async function actionTicketButton(
+  newTicketMessage: Message,
+  user: User,
+  ticketChannel: TextChannel
+) {
+  try {
+    const buttonAction = await newTicketMessage.awaitMessageComponent({
+      time: 3_600_000,
+    });
+
+    if (buttonAction.customId === "ticketDelete") {
+      sendDebug(`Suppression du ticket de ${user.tag}.`);
+
+      sentry(
+        buttonAction.user.client,
+        "Ticket",
+        `${whiteCheckMark} Suppression d'un ticket (${ticketChannel.name})`,
+        buttonAction.user
+      );
+
+      deleteTicketChannelText(ticketChannel, user);
+
+      return;
+    }
+  } catch (e) {
+    sendError(`Une erreur est survenur sur les actions des tickets (${e}).`);
   }
 }
